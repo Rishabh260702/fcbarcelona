f4d1f7c16a4a092310f830e3158cf1ef3520eaa3
Rishabh Pandey <82309798+Rishabh260702@users.noreply.github.com>
2021-10-25 23:01:11 +0530

    done

---
+++ logic.js
@@ -0,0 +1,15 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=="admin"&&password=="admin")
+    {
+        alert("Admin entry");
+        return false;
+    }
+    else
+    {
+        alert("Pubic Entry")
+    }
+    
+}
